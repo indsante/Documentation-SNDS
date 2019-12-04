9d5243bcfefe712d5ed74d45426fc978ddf6842a
El Oualydy Salma <salma.eloualydy@gmail.com>
2019-12-04 17:02:14 +0000

    Ordre alphabétique de la barre latérale de navigation

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,56 +1,21 @@
 
-const fs = require('fs');
-
-function listMarkdownFilesInDirectory(directoryName) {
-    return fs
-        .readdirSync('./' + directoryName)
-        .sort(function (a, b) {
-            return a.toLowerCase().localeCompare(b.toLowerCase());
-        })
-        .filter(function (filename) {
-            return filename !== 'README.md';
-        })
-        .filter(function (filename) {
-            return filename.slice(-3) === '.md';
-        })
-        .map(function (filename) {
-            return '/' + directoryName + '/' + filename.slice(0, -3);
-        });
-}
-
-function listSubDirectories(directoryName) {
-    return fs
-        .readdirSync('./' + directoryName)
-        .filter(function (filename) {
-            return fs.lstatSync('./' + directoryName + '/' + filename).isDirectory();
-        })
-}
-
-
-function getSidebarGroup(directoryPath) {
-    const lastDirectory = directoryPath.split("/").pop();
-    const sidebarGroup = {};
-
-    sidebarGroup["title"] = lastDirectory.charAt(0).toUpperCase() + lastDirectory.slice(1);
-
-    if (fs.existsSync('./' + directoryPath + '/README.md')) {
-        sidebarGroup["path"] = '/' + directoryPath + '/';
-    }
-    const children = [];
-    for (subDirectory of listSubDirectories(directoryPath)) {
-        children.push(getSidebarGroup(directoryPath + '/' + subDirectory));
-    }
-    sidebarGroup["children"] = children.concat(listMarkdownFilesInDirectory(directoryPath));
-    return sidebarGroup
-}
-
+const sb = require('./sidebar');
 
 const sidebar = [
-    getSidebarGroup("introduction"),
-    getSidebarGroup("fiches"),
-    getSidebarGroup("glossaire"),
-    getSidebarGroup("ressources"),
-    getSidebarGroup("tables"),
+    {
+        title: 'Introduction',
+        path: '/introduction/',
+        children: [
+          '/introduction/01-snds',
+          '/introduction/02-bases-snds',
+          '/introduction/03-acces-snds',
+          '/introduction/04-recherche-snds'
+        ]
+    },
+    sb.getSidebarGroup("fiches"),
+    sb.getSidebarGroup("glossaire"),
+    sb.getSidebarGroup("ressources"),
+    sb.getSidebarGroup("tables"),
     {
         title: 'Contribuer',
         path: '/contribuer/',
@@ -80,7 +45,7 @@ module.exports = {
             {text: 'Groupe Meetup', link: 'https://www.meetup.com/fr-FR/Health-Data-Hub/'},
         ],
         sidebar: sidebar,
-        sidebarDepth: 1,
+        sidebarDepth: 0,
         lastUpdated: 'Dernière mise à jour',
         algolia: {
             apiKey: 'b1d88a4e2413821b4ebe7441ecc95a97',
--- fiches/precarite.md
+++ fiches/precarite.md
@@ -1,4 +1,4 @@
-# Les publics précaires
+# Publics précaires
 <!-- SPDX-License-Identifier: MPL-2.0 -->
 
 L'objectif de cette fiche est de décrire comment repérer les publics précaires dans le SNDS.
