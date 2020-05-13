9cac66bc20833bcdbd28f749417b2f50331416f3
Lorien Benda <lorien.benda@gmail.com>
2020-05-13 10:42:54 +0200

    correction fichier config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -38,7 +38,7 @@ const sidebar = [
                     "/contribuer/Guide_utilisation_des_outils/Modifier_une_fiche",
                     "/contribuer/Guide_utilisation_des_outils/Creer_une_fiche",
                     "/contribuer/Guide_utilisation_des_outils/inclure_image",
-                    "/contribuer/Guide_utilisation_des_outils/partage_document"
+                    "/contribuer/Guide_utilisation_des_outils/partage_document",
                     "/contribuer/Guide_utilisation_des_outils/contribution_tables",
                     "/contribuer/Guide_utilisation_des_outils/Forum",
                     "/contribuer/Guide_utilisation_des_outils/developpement_local",
