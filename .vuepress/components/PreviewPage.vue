f834369e8d055255f2535eec1a5b7e868dab83a7
Salma <salma.eloualydy@gmail.com>
2021-07-02 17:52:39 +0200

    creating preview component and test page

---
+++ .vuepress/components/PreviewPage.vue
@@ -0,0 +1,42 @@
+<template>
+<span >
+    <a :href="link" id="trig1">{{text}}</a>
+    <iframe id='ifrm1' name='ifrm1' class="preview-page" :src="link"></iframe>
+    
+</span>
+</template>
+
+<script>
+export default {
+name : 'PreviewPage',
+props: {
+    link : String,
+    text : String
+}
+}
+</script>
+
+<style>
+
+.preview-page {
+    width:500px; height:400px;
+    -webkit-transform:scale(.25);
+    -ms-transform:scale(.5);
+    transform:scale(.7);
+    -webkit-transform-origin:0 0; 
+    -ms-transform-origin:0 0; 
+    transform-origin:0 0; 
+    border:4px solid #42b983;
+    margin:0 0 0 0;
+}
+
+#ifrm1 {
+  display : none;
+  visibility : hidden;
+  position: absolute;
+}
+#trig1:hover + iframe {
+  display : block;
+  visibility : visible;
+}
+</style>
