8583394fc8eb27ac1861a1300bbb6c88a550027c
Salma <salma.eloualydy@gmail.com>
2021-07-07 17:08:18 +0200

    adaptation script de preview à un environnement linux

--- transform-preview-links.sh
+++ transform-preview-links.sh
@@ -13,7 +13,7 @@ grep -ERio --exclude-dir={node_modules,.vuepress,files,contribuer} --include \*.
     output=$(echo "$seq" | sed -e 's:\[:<PreviewPage text=":g' -e 's:\](:" link=":g' -e 's:\.md):\.html" \/>:g') 
     escaped_output=$(printf '%s\n' "$output" | sed -e 's/[]\/$*.^[]/\\&/g')
     escaped_input=$(printf '%s\n' "$seq" | sed -e 's/[]\/$*.^[]/\\&/g')
-    sed -i '' -e "s|$escaped_input|$escaped_output|g" "$file"
+    sed -i -e "s|$escaped_input|$escaped_output|g" "$file"
 done
 echo ___INFO: création des preview pour les références externes aux tables___ 
 grep -ERio --exclude-dir={node_modules,.vuepress,files,contribuer} --include \*.md '\[[^\/]*\]\(\.\.\/tables\/[^\/]*\/[^\/]*\.md\)' . | wc -l
@@ -28,7 +28,7 @@ grep -ERio --exclude-dir={node_modules,.vuepress,files,contribuer} --include \*.
     output=$(echo "$seq" | sed -e 's:\[:<PreviewPage text=":g' -e 's:\](:" link=":g' -e 's:\.md):\/" \/>:g' -e 's:\*\*::g' -e 's:README\/::g' -e 's:DCIR\/::g' -e 's:BENEFICIAIRE\/::g' -e 's:DCIR_DCIRS\/::g' -e 's:PMSI\/PMSI MCO\/::g' -e 's:PMSI\/PMSI%20MCO\/::g' -e 's:PMSI\/PMSI HAD\/::g' -e 's:PMSI\/PMSI SSR\/::g' -e 's:PMSI\/PMSI RIM-P\/::g' -e 's:DAMIR\/::g' -e 's:DCIRS\/::g' -e 's:Causes de décès\/::g' -e 's:Causes%20de%20décès::g' -e 's:CARTOGRAPHIE_PATHOLOGIES\/::g' -e 's:EGB\/::g' ) 
     escaped_output=$(printf '%s\n' "$output" | sed -e 's/[]\/$*.^[]/\\&/g')
     escaped_input=$(printf '%s\n' "$seq" | sed -e 's/[]\/$*.^[]/\\&/g')
-    sed -i '' -e "s|$escaped_input|$escaped_output|g" "$file"
+    sed -i -e "s|$escaped_input|$escaped_output|g" "$file"
 done
 echo ___INFO: création des preview pour les références internes au glossaire___ 
 grep -ERio --exclude-dir={node_modules,.vuepress,files} --include \*.md '\[[^\[]+\]\([^\/\[]+\.md\)' ./glossaire/ | wc -l
@@ -43,8 +43,7 @@ grep -ERio --exclude-dir={node_modules,.vuepress,files} --include \*.md '\[[^\[]
     output=$(echo "$seq" | sed -e 's:\[:<PreviewPage text=":g' -e 's:\](:" link=":g' -e 's:\.md):\.html" \/>:g')
     escaped_output=$(printf '%s\n' "$output" | sed -e 's/[]\/$*.^[]/\\&/g')
     escaped_input=$(printf '%s\n' "$seq" | sed -e 's/[]\/$*.^[]/\\&/g')
-    sed -i '' -e "s|$escaped_input|$escaped_output|g" "$file"
-    output2=$(echo "RESULT $seq END" | sed -e "s|$escaped_input|$escaped_output|g")
+    sed -i -e "s|$escaped_input|$escaped_output|g" "$file"
 done
 echo ___INFO: création des preview pour les références internes aux tables___
 grep -ERio --exclude-dir={node_modules,.vuepress,files} --include \*.md '\[[^\/]*\]\(\/tables\/[^\/]*)' ./tables/ | wc -l
@@ -55,5 +54,5 @@ grep -ERio --exclude-dir={node_modules,.vuepress,files} --include \*.md '\[[^\/]
     output=$(echo "$seq" | sed -e 's:\[:<PreviewPage text=":g' -e 's:\](:" link=":g' -e 's:):" \/>:g' -e 's:DCIR\/::g' -e 's:BENEFICIAIRE\/::g' -e 's:DCIR_DCIRS\/::g' -e 's:PMSI\/PMSI MCO\/::g' -e 's:PMSI\/PMSI%20MCO\/::g' -e 's:PMSI\/PMSI HAD\/::g' -e 's:PMSI\/PMSI SSR\/::g' -e 's:PMSI\/PMSI RIM-P\/::g' -e 's:DAMIR\/::g' -e 's:DCIRS\/::g' -e 's:Causes de décès\/::g' -e 's:Causes%20de%20décès::g' -e 's:CARTOGRAPHIE_PATHOLOGIES\/::g' -e 's:EGB\/::g' ) 
     escaped_output=$(printf '%s\n' "$output" | sed -e 's/[]\/$*.^[]/\\&/g')
     escaped_input=$(printf '%s\n' "$seq" | sed -e 's/[]\/$*.^[]/\\&/g')
-    sed -i '' -e "s|$escaped_input|$escaped_output|g" "$file"
+    sed -i -e "s|$escaped_input|$escaped_output|g" "$file"
 done
